6d64c24e0d7ec2330fefd8aebc908e81427cf30c
Jessdoingit <piercej6744@student.faytechcc.edu>
2026-06-26 21:41:03 -0400

    Add files via upload

---
+++ P1LAB1_PierceJessica.py
@@ -0,0 +1,10 @@
+# Jessica Pierce
+# 06/26/2026
+# P1LAB1
+# Using first and last name, and display of message.
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
